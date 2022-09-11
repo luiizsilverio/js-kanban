f5e8374c3a6f82296411c4c684b6c79453e6258d
luiizsilverio <luiiz.silverio@gmail.com>
2022-09-11 09:41:36 -0300

    drag and drop completed

--- script.js
+++ script.js
@@ -56,10 +56,6 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  // console.log('columnEl:', columnEl);
-  // console.log('column:', column);
-  // console.log('item:', item);
-  // console.log('index:', index);
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
@@ -78,11 +74,11 @@ function dragEnter(column) {
 }
 
 function dragLeave(column) {
-  listColumns[column].classList.remove("over");
+  // listColumns[column].classList.remove("over");
 }
 
 function allowDrop(ev) {
-  ev.preventDefault();  
+  ev.preventDefault();  // o padrão é não permitir
 }
 
 function drop(ev) {
@@ -90,7 +86,28 @@ function drop(ev) {
   listColumns.forEach((column) => column.classList.remove("over"));
 
   const parent = listColumns[currentColumn];
-  parent.appendChild(draggedItem);
+  parent.appendChild(draggedItem);  
+  rebuildArrays();
+}
+
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
@@ -125,8 +142,8 @@ function updateDOM() {
   })
 
   // Run getSavedColumns only once, Update Local Storage
-
-
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
 
 // On Load
